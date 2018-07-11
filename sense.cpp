ec2a52d554e1596bcb97ed99de076b43f1d64086
Kang Shin <moo@mootower.attlocal.net>
2018-07-11 10:48:30 -0400

    optimized sense

--- sense.cpp
+++ sense.cpp
@@ -2,39 +2,25 @@
 
 using namespace std;
 
-// OPTIMIZATION: Pass larger variables by reference
-vector< vector <float> > sense(char color, vector< vector <char> > grid, vector< vector <float> > beliefs,  float p_hit, float p_miss) 
+vvector< vector <float> > sense_improved(char color, 
+	vector< vector <char> > &grid, 
+	vector< vector <float> > &beliefs, 
+	float p_hit,
+	float p_miss)
 {
-	// OPTIMIZATION: Is the newGrid variable necessary?
-  	// Could the beliefs input variable be updated directly?
-  	vector< vector <float> > newGrid;
-	vector<float> row, newRow;
-
-	float prior, p;
-
-	char cell;
-
-	int i, j, height, width;
+	int height, width;
 	height = grid.size();
-	width = grid.size();
+	width = grid[0].size();
 
-	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
-		for (j=0; j<grid[0].size(); j++) {
-          	// OPTIMIZATION: Which of these variables are 					needed?
-			prior = beliefs[i][j];
-			cell = grid[i][j];
-			if (cell == color) {
-				p = prior * p_hit;
+	for(int i = 0; i < height; ++i){
+		for(int j = 0; j< width; ++j){
+			if(grid[i][j] == color){
+				beliefs[i][j] = beliefs[i][j] * p_hit;
 			}
-            // OPTIMIZATION: if else statements might be 
-          	// 	faster than two if statements
-			if (cell != color) {
-				p = prior * p_miss;
+			else{
+				beliefs[i][j] = beliefs[i][j] ( p_miss);
 			}
-			newRow.push_back(p);
 		}
-		newGrid.push_back(newRow);
 	}
-	return newGrid;
-}
+	return beliefs;
+} 
