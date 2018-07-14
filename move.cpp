5ca3d2243d198b40f705b1a109402025d22118fb
Kang Shin <kshin33@gmail.com>
2018-07-14 13:43:26 -0400

    optimized move

--- move.cpp
+++ move.cpp
@@ -3,28 +3,21 @@
 
 using namespace std;
 
-vector< vector <float> > move(int dy, int dx, 
-	vector < vector <float> > beliefs) 
+vector< vector <float> > move_improved(int dy, int dx, vector < vector <float> > &beliefs) 
 {
 	int height, width;
 	height = beliefs.size();
 	width = beliefs[0].size();
 
-	float belief;
-	vector < vector <float> > newGrid;
+	vector < vector <float> >  newGrid = zeros_improved(height, width);
 
-	newGrid = zeros(height, width);
-
-	vector <float> row, newRow;
 	int i, j, new_i, new_j;
-	
-  	for (i=0; i<height; i++) {
-		for (j=0; j<width; j++) {
+
+	for (i=0; i<height; ++i) {
+		for (j=0; j<width; ++j) {
 			new_i = (i + dy + height) % height;
 			new_j = (j + dx + width)  % width;
-			belief = beliefs[i][j];
-
-			newGrid[new_i][new_j] = belief;
+			newGrid[new_i][new_j] = beliefs[i][j];
 		}
 	}
 	return newGrid;
