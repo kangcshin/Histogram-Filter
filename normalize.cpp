75cc28ab8c15e4c7412dc20a01adec2972797f38
Kang Shin <kshin33@gmail.com>
2018-07-14 13:41:41 -0400

    Optimized normalize

--- normalize.cpp
+++ normalize.cpp
@@ -1,25 +1,23 @@
 #include "headers/normalize.h"
 using namespace std;
 
-vector< vector<float> > normalize(vector< vector <float> > grid) {
+vector< vector<float> > normalize_improved(vector< vector <float> > &grid) {
+
 	float total = 0.0;
 	int rows = grid.size();
 	int cols = grid[0].size();
 	int i, j;
 
-	for (i = 0; i < rows; ++i)
-	{
-		for (j = 0; j< cols; ++j)
-		{
+	for (i = 0; i < rows; ++i){
+		for (j = 0; j< cols; ++j){
 			total += grid[i][j];
 		}
 	}
 
-	for (i = 0; i < rows; ++i) {
-		for (j=0; j< cols; ++j) {
+	for (i = 0; i < rows; ++i){
+		for (j=0; j< cols; ++j){
 			grid[i][j] = grid[i][j] / total;
 		}
 	}
-
 	return grid;
 }
